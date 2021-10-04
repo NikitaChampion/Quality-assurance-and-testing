4c7758fd42b0948a942ab07018a4677087523708
Nikita Igumnov <nkigumnov@yandex.ru>
2021-10-04 13:10:03 +0300

    Update hw2

--- HW2/tests/root/gcd/GCDTest.java
+++ HW2/tests/root/gcd/GCDTest.java
@@ -2,119 +2,101 @@ package root.gcd;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GCDTest {
-    public GCD gcdInstance = new GCD();
-    public NewGCD newGCDInstance = new NewGCD();
+    public GCD gcd = new GCD();
 
     @Test
     void positiveTest() {
-        for (int x = 1; x <= 1000; ++x) {
-            for (int y = 1; y <= 1000; ++y) {
-                assertEquals(gcdInstance.gcd(x, y), newGCDInstance.gcd(x, y));
-            }
-        }
+        assertEquals(1, gcd.gcd(1, 2));
+        assertEquals(1, gcd.gcd(2, 1));
+
+        assertEquals(2, gcd.gcd(2, 4));
+        assertEquals(2, gcd.gcd(4, 2));
+
+        assertEquals(4, gcd.gcd(4, 8));
+        assertEquals(4, gcd.gcd(8, 4));
     }
 
     @Test
     void negativeTest() {
-        for (int x = 1; x <= 1000; ++x) {
-            for (int y = 1; y <= 1000; ++y) {
-                assertEquals(gcdInstance.gcd(-x, -y), newGCDInstance.gcd(-x, -y));
-                assertEquals(gcdInstance.gcd(-x, y), newGCDInstance.gcd(-x, y));
-                assertEquals(gcdInstance.gcd(x, -y), newGCDInstance.gcd(x, -y));
-            }
-        }
+        assertEquals(1, gcd.gcd(-1, 2));
+        assertEquals(1, gcd.gcd(-2, 1));
+
+        assertEquals(2, gcd.gcd(2, -4));
+        assertEquals(2, gcd.gcd(4, -2));
+
+        assertEquals(4, gcd.gcd(-4, -8));
+        assertEquals(4, gcd.gcd(-8, -4));
     }
 
     @Test
     void zeroTest() {
-        for (int i = Integer.MIN_VALUE + 1; i <= Integer.MAX_VALUE - 1; ++i) {
-            if (i > 0) {
-                assertEquals(gcdInstance.gcd(0, i), i);
-                assertEquals(gcdInstance.gcd(0, i), i);
-            } else {
-                assertEquals(gcdInstance.gcd(i, 0), -i);
-                assertEquals(gcdInstance.gcd(i, 0), -i);
-            }
-        }
+        assertEquals(1, gcd.gcd(0, 1));
+        assertEquals(1, gcd.gcd(1, 0));
+
+        assertEquals(2, gcd.gcd(0, -2));
+        assertEquals(2, gcd.gcd(-2, 0));
+
+        assertEquals(4, gcd.gcd(0, -4));
+        assertEquals(4, gcd.gcd(-4, 0));
     }
 
     @Test
-    void primeTest() {
-        for (int x = 1; x <= 1000; ++x) {
-            for (int y = 1; y <= 1000; ++y) {
-                if (newGCDInstance.gcd(x, y) == 1) {
-                    assertEquals(gcdInstance.gcd(x, y), 1);
-                }
-            }
-        }
+    void coprimeTest() {
+        assertEquals(1, gcd.gcd(91, 99));
+        assertEquals(1, gcd.gcd(91, 101));
+        assertEquals(1, gcd.gcd(91, 103));
+        assertEquals(1, gcd.gcd(91, 107));
+        assertEquals(1, gcd.gcd(91, 109));
     }
 
     @Test
     void equalTest() {
-        for (int i = Integer.MIN_VALUE + 1; i <= Integer.MAX_VALUE - 1; ++i) {
-            if (i >= 0) {
-                assertEquals(gcdInstance.gcd(i, i), i);
-            } else {
-                assertEquals(gcdInstance.gcd(i, i), -i);
-            }
-        }
+        assertEquals(4, gcd.gcd(4, 4));
+
+        assertEquals(2, gcd.gcd(-2, -2));
+
+        assertEquals(10000, gcd.gcd(-10000, -10000));
+
+        assertEquals(0, gcd.gcd(0, 0));
     }
 
     @Test
-    void notEqualDivTest() {
-        for (int i = -1000; i <= 1000; ++i) {
-            if (i == 0) {
-                continue;
-            }
-            for (int j = -1000; j <= 1000; ++j) {
-                if (j == 0 || j == 1) {
-                    continue;
-                }
-                if (i * j / j == i) {
-                    if (i >= 0) {
-                        assertEquals(gcdInstance.gcd(i * j, i), i);
-                        assertEquals(gcdInstance.gcd(i, i * j), i);
-                    } else {
-                        assertEquals(gcdInstance.gcd(i * j, i), -i);
-                        assertEquals(gcdInstance.gcd(i, i * j), -i);
-                    }
-                }
-            }
-        }
+    void divisionByTest() {
+        assertEquals(2, gcd.gcd(2, 8));
+        assertEquals(2, gcd.gcd(8, 2));
+
+        assertEquals(2, gcd.gcd(2, 1000));
+        assertEquals(2, gcd.gcd(1000, 2));
     }
 
     @Test
     void notEqualTest() {
-        int k = 2;
-        for (int i = -1000; i <= 1000; ++i) {
-            if (i == 0) {
-                continue;
-            }
-            for (int j = -1000; j <= 1000; ++j) {
-                if (j == 0 || i == j) {
-                    continue;
-                }
-                assertTrue(gcdInstance.gcd(k * i, k * j) > 1);
-            }
-        }
+        assertEquals(3, gcd.gcd(6, 21));
+        assertEquals(3, gcd.gcd(21, 6));
+
+        assertEquals(2, gcd.gcd(4, 6));
+        assertEquals(2, gcd.gcd(6, 4));
+    }
+
+    @Test
+    void fibonacciTest() {
+        // Fib[46], Fib[45]
+        assertEquals(1, gcd.gcd(1836311903, 1134903170));
     }
 
     @Test
     void overflowTest() {
-        for (int i = Integer.MIN_VALUE; i <= Integer.MAX_VALUE - 1; ++i) {
-            assertEquals(gcdInstance.gcd(Integer.MIN_VALUE, i),
-                    newGCDInstance.gcd(Integer.MIN_VALUE, i));
-            assertEquals(gcdInstance.gcd(i, Integer.MIN_VALUE),
-                    newGCDInstance.gcd(i, Integer.MIN_VALUE));
-        }
-        for (int i = Integer.MIN_VALUE; i <= Integer.MAX_VALUE - 1; ++i) {
-            assertEquals(gcdInstance.gcd(Integer.MAX_VALUE, i),
-                    newGCDInstance.gcd(Integer.MAX_VALUE, i));
-            assertEquals(gcdInstance.gcd(i, Integer.MAX_VALUE),
-                    newGCDInstance.gcd(i, Integer.MAX_VALUE));
-        }
+        assertEquals(Integer.MAX_VALUE, gcd.gcd(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        assertTimeoutPreemptively(Duration.ofSeconds(2), () ->
+                assertEquals(1, gcd.gcd(Integer.MIN_VALUE, Integer.MAX_VALUE)));
+        assertTimeoutPreemptively(Duration.ofSeconds(2), () ->
+                assertEquals(1, gcd.gcd(Integer.MAX_VALUE, Integer.MIN_VALUE)));
+        assertTimeoutPreemptively(Duration.ofSeconds(2), () ->
+                assertEquals((long) Math.pow(2, 31), gcd.gcd(Integer.MIN_VALUE, Integer.MIN_VALUE)));
     }
 }
